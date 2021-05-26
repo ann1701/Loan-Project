c4f1f5f8cefa498b39e65193c54eb3865f20cb4f
annim <annim@DESKTOP-00QV2OS>
2021-05-26 11:31:49 +0530

    first changes

---
+++ 25.0.ProjectLoanServer/src/main/java/com/example/main/Application.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("This is Our Eureka Server");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
